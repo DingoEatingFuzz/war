daf2e17c8ad6e67b4af1b1ceb3eda43e56cb4ceb
Michael Lange <dingoeatingfuzz@gmail.com>
2021-05-07 23:38:30 -0700

    Create a war in main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import './style.css'
 import { Deck } from './deck'
+import War from './war'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 const deck = new Deck();
 deck.shuffle();
 deck.print();
+
+const war = new War(deck, 2);
+console.log(war.play());
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
