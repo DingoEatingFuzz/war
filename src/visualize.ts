e88196c7c4cdd59e8a49dd892a82dbe2811fea72
Michael Lange <dingoeatingfuzz@gmail.com>
2021-07-05 16:54:46 -0700

    Start of the svg visualization of war

--- src/visualize.ts
+++ src/visualize.ts
@@ -1,7 +1,68 @@
-import { Round } from './war'
+import { Round, Play } from './war'
+import { Card } from './deck'
+import * as d3 from 'd3'
+
+export function aceTrace(rounds:Array<Round>, elem:SVGElement):void {
+  const viz = d3.select(elem);
+
+  const deckCardWidth = 3;
+  const roundHeight = 15;
+  const roundGap = 2;
+
+  const reds = d3.quantize(d3.interpolateLab('#cc7050', '#c90000'), 14);
+  const blacks = d3.quantize(d3.interpolateLab('#3a6266', '#07090c'), 14);
+
+  viz
+    .attr('class', 'war-viz')
+    .attr('height', rounds.length * (roundHeight + roundGap))
+    .attr('width', 1);
+
+  // Each round gets a row
+  const $rounds = viz.selectAll('.round')
+    .data(rounds)
+    .enter()
+      .append('g')
+      .attr('class', 'round')
+      .attr('transform', (_:Round, i:number) => `translate(0, ${i * (roundHeight + roundGap)})`);
+
+  const $decks = $rounds
+    .selectAll('.deck')
+    .data((d:Round) => d.lastMatch.plays)
+    .enter()
+      .append('g')
+      .attr('class', 'deck')
+      .attr('transform', (_:any, i:number) => `scale(${i === 0 ? '-1' : '1'} 1) translate(${i === 1 ? roundGap : 0} 0)`);
+
+  $decks
+    .selectAll('.deck-card')
+    .data((d:Play|null) => d ? d.hand : [])
+    .enter()
+      .append('rect')
+      .attr('class', 'deck-card')
+      .attr('x', (_:any, i:number) => i * deckCardWidth)
+      .attr('width', deckCardWidth)
+      .attr('height', roundHeight)
+      .style('fill', (d:Card) => false ? reds[d.rank] : blacks[d.rank]);
+
+  // Each round has the following components:
+  // 1. the deck of each player
+  // 2. all the matches
+
+  // Each match has the following components:
+  // 1. the face up card of each player
+  // 2. the face down cards (in the event of a war)
+}
+
+function normalizeDecks(plays:Array<Play>): Array<Play|null> {
+  console.log(plays.map(p => p.player.position));
+  const highPlayer:number = plays.reduce((high:number, p:Play) => Math.max(high, p.player.position), 0);
+  return new Array(highPlayer).fill(null).map((_:any, i:number) => {
+    return plays.find(p => p.player.position === i) || null;
+  });
+}
 
 export function asciiOrderedList(rounds:Array<Round>):string {
-  return rounds.map(round => {
+  const lis = rounds.map(round => {
     const match = round.matches[0]
     const tie = match.plays[0].activeCard.equalTo(match.plays[1].activeCard)
     const playStrs = match.plays.map(p => {
@@ -14,4 +75,5 @@ export function asciiOrderedList(rounds:Array<Round>):string {
     })
     return `<li class="${tie ? 'tie' : ''}">${playStrs.join(' v. ')}</li>`
   }).join('\n')
+  return `<ol>${lis}</ol>`;
 }
