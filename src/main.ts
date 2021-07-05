6072a3f8d4e2fe0179361e227a84e9fe0dd67e23
Michael Lange <dingoeatingfuzz@gmail.com>
2021-07-05 16:54:58 -0700

    Organize the main file just a tiny bit

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import './style.css'
 import { Deck } from './deck'
 import War, { Shuffles, Round } from './war'
 import { stats, zip } from './utils'
+import { asciiOrderedList, aceTrace } from './visualize';
 
 import * as Plot from '@observablehq/plot'
+
+enum Mode {
+  Stats,
+  Viz
+}
 
 const time = (label:string, fn:Function):any => {
   performance.mark(label + '-s');
@@ -18,20 +24,29 @@ function serialize(rounds:Array<Round>):string {
   return time('war-stringify', () => JSON.stringify(obj));
 }
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>('#app');
+
+const mode = Mode.Viz;
+
+const games:Array<Array<Round>> = [];
+let game:Array<Round> = [];
 
 const deck = new Deck();
 deck.shuffle();
 
-const games:Array<Array<Round>> = [];
-for (let i = 0; i < 100; i++) {
+if (mode === Mode.Stats) {
+  for (let i = 0; i < 100; i++) {
+    const war = new War(deck, 2, Shuffles.FisherYates);
+    games.push(war.play());
+    const str = serialize(games[0]);
+    time('war-parse', () => JSON.parse(str));
+  }
+} else if (mode === Mode.Viz) {
   const war = new War(deck, 2, Shuffles.FisherYates);
-  games.push(war.play());
-  const str = serialize(games[0]);
-  time('war-parse', () => JSON.parse(str));
+  game = war.play();
 }
 
-app.innerHTML = `
+const statsPage = `
   <main>
     <h1>100 games of war</h1>
 
@@ -56,65 +71,77 @@ app.innerHTML = `
   </main>
 `
 
-const perfData = performance.getEntriesByType('measure')
-const playHistogram = Plot.plot({
-  grid: true,
-  facet: {
-    data: perfData,
-    y: 'name',
-  },
-  fy: {
-    domain: ['war-play', 'war-serialize', 'war-stringify', 'war-parse'],
-  },
-  y: {
-    type: 'sqrt',
-  },
-  marks: [
-    Plot.rectY(
-      perfData,
-      Plot.binX(
-        { y: 'count' },
-        { x: 'duration', fill: 'name', thresholds: 75 },
-      )
-    ),
-    Plot.ruleY([0]),
-  ]
-});
+const vizPage = `<main><div id='warViz'>${asciiOrderedList(game)}</div></main>`;
+const vizPage2 = `<main><svg id='warViz'></svg></main>`;
 
-const plotData = zip({
-  game: games.map(g => stats(g)),
-  play: performance.getEntriesByName('war-play'),
-  serialize: performance.getEntriesByName('war-serialize'),
-  stringify: performance.getEntriesByName('war-stringify'),
-  parse: performance.getEntriesByName('war-parse'),
-})
+if (app) {
+  app.innerHTML = mode === Mode.Stats ? statsPage : vizPage2
+}
 
-const scatterPoints = expandPlotData(plotData);
-
-const playPlot = Plot.plot({
-  marks: [
-    Plot.dot(
-      scatterPoints,
-      { x: 'rounds', y: 'duration', fill: 'series', fillOpacity: 0.7 }
-    )
-  ]
-});
-
-const byMetric = document.querySelector('#timeByMetric');
-if (byMetric) byMetric.appendChild(playHistogram);
-
-const timeByRounds = document.querySelector('#timeByRounds');
-if (timeByRounds) timeByRounds.appendChild(playPlot);
-
-// Turn the table of objects into a single set of data points with a series label
-function expandPlotData(data:Array<any>) {
-  const points:Array<any> = [];
-  data.forEach(d => {
-    points.push({ rounds: d.game.count, duration: d.play.duration, series: 'war-play' });
-    points.push({ rounds: d.game.count, duration: d.serialize.duration, series: 'war-serialize' });
-    points.push({ rounds: d.game.count, duration: d.stringify.duration, series: 'war-stringify' });
-    points.push({ rounds: d.game.count, duration: d.parse.duration, series: 'war-parse' });
+if (mode === Mode.Stats) {
+  const perfData = performance.getEntriesByType('measure')
+  const playHistogram = Plot.plot({
+    grid: true,
+    facet: {
+      data: perfData,
+      y: 'name',
+    },
+    fy: {
+      domain: ['war-play', 'war-serialize', 'war-stringify', 'war-parse'],
+    },
+    y: {
+      type: 'sqrt',
+    },
+    marks: [
+      Plot.rectY(
+        perfData,
+        Plot.binX(
+          { y: 'count' },
+          { x: 'duration', fill: 'name', thresholds: 75 },
+        )
+      ),
+      Plot.ruleY([0]),
+    ]
   });
 
-  return points;
+  const plotData = zip({
+    game: games.map(g => stats(g)),
+    play: performance.getEntriesByName('war-play'),
+    serialize: performance.getEntriesByName('war-serialize'),
+    stringify: performance.getEntriesByName('war-stringify'),
+    parse: performance.getEntriesByName('war-parse'),
+  })
+
+  const scatterPoints = expandPlotData(plotData);
+
+  const playPlot = Plot.plot({
+    marks: [
+      Plot.dot(
+        scatterPoints,
+        { x: 'rounds', y: 'duration', fill: 'series', fillOpacity: 0.7 }
+      )
+    ]
+  });
+
+  const byMetric = document.querySelector('#timeByMetric');
+  if (byMetric) byMetric.appendChild(playHistogram);
+
+  const timeByRounds = document.querySelector('#timeByRounds');
+  if (timeByRounds) timeByRounds.appendChild(playPlot);
+
+  // Turn the table of objects into a single set of data points with a series label
+  function expandPlotData(data:Array<any>) {
+    const points:Array<any> = [];
+    data.forEach(d => {
+      points.push({ rounds: d.game.count, duration: d.play.duration, series: 'war-play' });
+      points.push({ rounds: d.game.count, duration: d.serialize.duration, series: 'war-serialize' });
+      points.push({ rounds: d.game.count, duration: d.stringify.duration, series: 'war-stringify' });
+      points.push({ rounds: d.game.count, duration: d.parse.duration, series: 'war-parse' });
+    });
+
+    return points;
+  }
 }
+
+const warViz:SVGElement|null = document.querySelector('#warViz');
+if (warViz) aceTrace(game, warViz);
